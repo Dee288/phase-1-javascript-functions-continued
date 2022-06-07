0c4a7a7e4a4e69fa68d4e868b2666d50c0ff127b
dee288 <dianamajala10@gmail.com>
2022-06-07 23:44:55 +0300

    commit tree 1964bb381363e1f57b3a94f3fecc88d17287e282

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity ="roller coaster"){
+console.log("This Saturday, I want to $(activity)")
+}
+
+function mondayWork(){
+    const activity ="go to the office"
+    console.log("This is Monday, I will $(activity)'")
+
+}
+
+function wrapAdjective(){
+
+}
